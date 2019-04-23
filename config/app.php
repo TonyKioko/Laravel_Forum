797986d15a40d25343b4cb675261194695dc4fc8
TonyKioko <tonykioko384@gmail.com>
2019-04-24 00:19:40 +0300

    markdown setup

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         AdamWathan\EloquentOAuthL5\EloquentOAuthServiceProvider::class,
+        GrahamCampbell\Markdown\MarkdownServiceProvider::class,
 
     ],
 
@@ -227,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'SocialAuth' => AdamWathan\EloquentOAuth\Facades\OAuth::class,
+        'Markdown' => GrahamCampbell\Markdown\Facades\Markdown::class,
 
     ],
 
--- resources/views/discussions/show.blade.php
+++ resources/views/discussions/show.blade.php
@@ -39,7 +39,7 @@
             </h3>
             <hr>
             <p class="text-center">
-                {{$d->content}}
+                {!!Markdown::convertToHtml($d->content)!!}
             </p>
 
         </div>
